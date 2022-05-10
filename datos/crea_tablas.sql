7955fab2f8212b1e7b7a05c257d700b58f4e981f
Julian Baez <Julianbsuarez@hotmail.com>
2022-05-11 00:15:33 +0200

    tablas sql

---
+++ datos/crea_tablas.sql
@@ -0,0 +1,16 @@
+CREATE TABLE "movimientos" (
+    "id"    INTEGER,
+    "date"  TEXT NOT NULL,
+    "time"  TEXT NOT NULL,
+    "moneda_from"   TEXT NOT NULL,
+    "cantidad_from" REAL,
+    "moneda_to" TEXT NOT NULL,
+    "cantidad_to"   REAL,
+    PRIMARY KEY("id" AUTOINCREMENT)
+);
+
+CREATE TABLE "wallet" (
+    "moneda"   TEXT NOT NULL,
+    "cantidad" REAL,
+    PRIMARY KEY("moneda")
+);
